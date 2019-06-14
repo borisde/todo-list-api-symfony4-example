cf023c0026a1f944276926aabec1f161c5d240da
BorisDe <brosd2004@gmail.com>
2019-06-14 15:10:42 +0300

    Install Behat and api context

---
+++ features/bootstrap/RestApiContext.php
@@ -0,0 +1,10 @@
+<?php
+use Imbo\BehatApiExtension\Context\ApiContext;
+use Behat\Symfony2Extension\Context\KernelDictionary;
+
+
+class RestApiContext extends ApiContext
+{
+    use KernelDictionary;
+
+}
